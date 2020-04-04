18d9e813413650b77e1a782b012cbcd6e5ea952b
gusya33 <v6qx8nnc3@gmail.com>
2020-04-04 13:13:07 +0900

    RMVC_add

---
+++ db/migrate/20200403234204_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.integer :user_id,               null: false, foreign_key: true
+      t.string :category_id,           null: false, foreign_key: true
+      t.string  :task_name,             null: false
+      t.timestamps
+    end
+  end
+end
